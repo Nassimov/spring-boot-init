3678f4eabb105b14d363963fd1359e690c34594f
Nassim <nacimessi10@outlook.fr>
2024-10-05 15:14:26 +0200

    test ok

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,39 @@
 package com.example.demo;
 
+import com.example.demo.controller.HelloController;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
-
+	@Autowired
+	private MockMvc mockMvc;
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplicationTests.class);
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		logger.info("hello() method was called");
+		// Perform GET request to the root endpoint ("/")
+		mockMvc.perform(get("/"))
+				.andExpect(status().isOk());
+		assert 2==2;
 	}
 
 }
